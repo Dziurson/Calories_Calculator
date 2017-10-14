7a31f015a0448a375c09eba160a45540213a6b1c
Jakub Dziwura <jakub.dziwura@gmail.com>
2017-10-14 22:47:32 +0200

    Initial commit

---
+++ app/src/main/java/pl/edu/agh/student/calcalc/CalCalcTimer.java
@@ -0,0 +1,71 @@
+package pl.edu.agh.student.calcalc;
+
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by jakub on 14.10.2017.
+ */
+
+public class CalCalcTimer {
+
+    private boolean isStarted;
+    private boolean isPaused;
+    private Date startDate;
+    private Date pausedDate;
+    private Timer timer;
+    private TimerTask timertask;
+    private SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss.SSS");
+    private TextView textViewToUpdate;
+
+
+    CalCalcTimer(final TextView textViewToUpdate) {
+        this.textViewToUpdate = textViewToUpdate;
+        isStarted = false;
+    }
+
+    public void start() {
+        timer = new Timer();
+        timertask = new TimerTask() {
+            @Override
+            public void run() {
+                if (!isPaused) {
+                    textViewToUpdate.setText(timeFormat.format(new Date(new Date().getTime() - startDate.getTime())));
+                }
+            }
+        };
+        startDate = new Date();
+        timer.scheduleAtFixedRate(timertask,startDate,10);
+        isStarted = true;
+        isPaused = false;
+    }
+
+    public void stop() {
+        timertask.cancel();
+        timer.cancel();
+        timer.purge();
+        isStarted = false;
+    }
+
+    public void resume() {
+        startDate = new Date(startDate.getTime() + new Date().getTime() - pausedDate.getTime());
+        isPaused = false;
+    }
+
+    public void pause() {
+        isPaused = true;
+        pausedDate = new Date();
+    }
+
+    public boolean isStarted() {
+        return isStarted;
+    }
+
+    public boolean isPaused() {
+        return isPaused;
+    }
+}
