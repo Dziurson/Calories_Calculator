bb187a4a69660cdfc930be2dc8502f8aa1290523
Jakub Dziwura <jakub.dziwura@gmail.com>
2017-10-16 11:45:41 +0200

    Refactored ActivityHelper class.

--- app/src/main/java/pl/edu/agh/student/calcalc/helpers/ActivityHelper.java
+++ app/src/main/java/pl/edu/agh/student/calcalc/helpers/ActivityHelper.java
@@ -1,5 +1,6 @@
 package pl.edu.agh.student.calcalc.helpers;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 
@@ -8,7 +9,7 @@ import android.content.Intent;
  */
 
 public class ActivityHelper {
-    public static void bringActivityToFront(Context activityContext, Class<?> activityClass){
+    public static void findOrCreateActivity(Context activityContext, Class<? extends Activity> activityClass){
         Intent intActivityHandler = new Intent(activityContext,activityClass);
         intActivityHandler.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         activityContext.startActivity(intActivityHandler);
